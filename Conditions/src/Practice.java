6b1cfe191c59bea90776ac091ea39f00cb2ad645
jadhavakshay29 <akshayjadhavaj2911@gmail.com>
2022-08-21 01:02:37 +0530

    practice

--- Conditions/src/Practice.java
+++ Conditions/src/Practice.java
@@ -4,121 +4,176 @@ public class Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//Data types-
+        // Data types-
 
-    //1byte --> 8bit
-    //bit means binary numbers are stored in 0 and 1 each of them called bits
-    // int a = 5;  // 4 byte 
-    // short b = 8;  //2 byte memory storage i.e. 16 bit memory
-    // byte c= 8;   //1byte
-    // long d =4;  // 8 byte hence can store more precise value
-    // double e = 23.5; // 8 bytes, double takes decimal values also has high precesion
-    // float f = 29.5f;  // we write f for float value to get recognize
-     
-    // char name = 'a';  // 2byte memory storage 
-    // boolean b1 = true; //boolean only takes either true or false value
-    // boolean b2 = false;
-    // System.out.println("sum of all integer data types \n"+ (a+b+c+d+e+f));
+        // 1byte --> 8bit
+        // bit means binary numbers are stored in 0 and 1 each of them called bits
+        // int a = 5; // 4 byte
+        // short b = 8; //2 byte memory storage i.e. 16 bit memory
+        // byte c= 8; //1byte
+        // long d =4; // 8 byte hence can store more precise value
+        // double e = 23.5; // 8 bytes, double takes decimal values also has high
+        // precesion
+        // float f = 29.5f; // we write f for float value to get recognize
 
-    // //how to fetch character 
-    // String str= sc.nextLine();
-    // char ch = str.charAt(2);
-    // System.out.println(ch);
- 
-//swapping of numbers -->
+        // char name = 'a'; // 2byte memory storage
+        // boolean b1 = true; //boolean only takes either true or false value
+        // boolean b2 = false;
+        // System.out.println("sum of all integer data types \n"+ (a+b+c+d+e+f));
 
-    // int a =5;
-    // int b = 3;
-    // int temp;
-    // temp = a; 
-    // a = b;     
-    // b = temp;
-    // System.out.println(a);
-    // System.out.println(b);
+        // //how to fetch character
+        // String str= sc.nextLine();
+        // char ch = str.charAt(2);
+        // System.out.println(ch);
 
-//char storing -->
+        // swapping of numbers -->
 
-    //  char ch = 'a';  //ASCII value of 'a' is 97 so system will get 97 + 3 & will print 100
-    //  int b = 3;
-    //  int add = 'a'+ b;  //this is valid too 
-    //  System.out.println(add);
- 
-//type conversion -->
+        // int a =5;
+        // int b = 3;
+        // int temp;
+        // temp = a;
+        // a = b;
+        // b = temp;
+        // System.out.println(a);
+        // System.out.println(b);
 
-     //  char ch = 'a';
-    //  int b = 5;
-    //  int i = ch;  /*this is called implicit conversion 
-    //                  in this 2 byte char is being saved in 4 byte memory so no data will be lost */
-    //  char expli = (char)b; // this is called explicit conversion 
-                       /*so char has 2 byte and int has 4 byte so char can't really be able to store 
-                        * 4 byte integer value so it'll show error 
-                        but; if we use (char) before it we'll be forcing char to store integer in it; and
-                        it'll store it but some data will be lost;
-                        so avoid this
-                        */
-      // int a = (int)5.3;  // explicit converison   
-      // System.out.println(a);  // but the output will be 5 only 
+        // char storing -->
 
-    //   double a = 6/4; // a is double so it'll store the double value but 6 and 4 are int so thier ans wil
-    //                   //be an int value i.e. 1 but will get stored in double a as 1.0 
-    //   int b = 6/4;  
-    //   double c = a+b;  // c = 1.0 + 1 =2.0
-    //   System.out.println(c); // o/p is 2.0
+        // char ch = 'a'; //ASCII value of 'a' is 97 so system will get 97 + 3 & will
+        // print 100
+        // int b = 3;
+        // int add = 'a'+ b; //this is valid too
+        // System.out.println(add);
 
-    // double a = 55.5;
-    // a = a % 10;  
-    // System.out.println(a); //o/p is 5.5
+        // type conversion -->
 
-//Q. to find out wether the character is uppercase, lowercase or not a character.
-     
-    //  System.out.println("enter character: ");
-    //  String name = sc.next();
-    //  char ch = name.charAt(0);
+        // char ch = 'a';
+        // int b = 5;
+        // int i = ch; /*this is called implicit conversion
+        // in this 2 byte char is being saved in 4 byte memory so no data will be lost
+        // */
+        // char expli = (char)b; // this is called explicit conversion
+        /*
+         * so char has 2 byte and int has 4 byte so char can't really be able to store
+         * 4 byte integer value so it'll show error
+         * but; if we use (char) before it we'll be forcing char to store integer in it;
+         * and
+         * it'll store it but some data will be lost;
+         * so avoid this
+         */
+        // int a = (int)5.3; // explicit converison
+        // System.out.println(a); // but the output will be 5 only
 
-    //  if(ch >= 97 && ch <= 122){
-    //     System.out.println(ch+ " is lower character");
-    //  }else if( ch >= 65 && ch <=90){
-    //     System.out.println(ch +" is uppercase character");
-    //  }else{
-    //     System.out.println(ch+ " is not a character");
-    //  }
+        // double a = 6/4; // a is double so it'll store the double value but 6 and 4
+        // are int so thier ans wil
+        // //be an int value i.e. 1 but will get stored in double a as 1.0
+        // int b = 6/4;
+        // double c = a+b; // c = 1.0 + 1 =2.0
+        // System.out.println(c); // o/p is 2.0
+
+        // double a = 55.5;
+        // a = a % 10;
+        // System.out.println(a); //o/p is 5.5
+
+        // Q. to find out wether the character is uppercase, lowercase or not a
+        // character.
+
+        // System.out.println("enter character: ");
+        // String name = sc.next();
+        // char ch = name.charAt(0);
+
+        // if(ch >= 97 && ch <= 122){
+        // System.out.println(ch+ " is lower character");
+        // }else if( ch >= 65 && ch <=90){
+        // System.out.println(ch +" is uppercase character");
+        // }else{
+        // System.out.println(ch+ " is not a character");
+        // }
+
+        // int x=5;
+        // int y=5;
+        // while((x=5)==y)
+        // {
+        // System.out.println("Hello");
+        // x++;
+        // y++;
+        // }
+        /*
+         * here the output will get print only one time cause
+         * x==5 will always be 5 in line 88
+         */
+
+        // sum of n numbers -->
+
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for(int i =1; i<=n; i++){
+        // sum = sum+i;
+        // }
+        // System.out.println("sum of n numbers is "+ sum);
+
+        // Fahrenheit to Celsius Table
+
+        /*
+         * Q. Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E)
+         * and Step Size (W),
+         * you need to convert all Fahrenheit values from Start to End at the gap of W,
+         * into their
+         * corresponding Celsius values and print the table.
+         */
+
+        // int startF = sc.nextInt(); // startF --> starting value of fahrenheit
+        // int endF = sc.nextInt(); // endF --> end value of fahrenheit
+        // int g = sc.nextInt(); //gap
+        // double c;
+        // while(startF <= endF){
+        // c = (startF-32)* 5/9;
+        // //int d = (int)c;
+        // System.out.println(startF+ " "+c);
+        // startF= startF + g;
+        // }
+
+        /*
+         * Q. Write a program to calculate the total salary of a person. The user has to
+         * enter
+         * the basic salary (an integer) and the grade (an uppercase character), and
+         * depending
+         * upon which the total salary is calculated as -
+         */
+
+        // int basic, hra, da, pf;
+        // int allow = 0;
+        // basic = sc.nextInt();
+        // String ch = sc.next();
+        // char grade = ch.charAt(0); // we assigned char here cause system wont understand A as A
+        // hra = basic * 20 / 100;
+        // da = basic * 50 / 100;
+        // pf = basic * 11 / 100;
+        // if (grade >= 65 && grade <= 90) { // 65 and 90 are ascii values of A nd B
+        //     if (grade == 'A') { // grade is turned into char so that it could unerstand char A nd B
+        //         allow = 1700;
+        //     } else if (grade == 'B') {
+        //         allow = 1500;
+        //     } else {
+        //         allow = 1300;
+        //     }
+        // } else {
+        //     System.out.println("not a valid letter");
+        // }
+        // int totalSalary = basic + hra + da + allow - pf;
+        // System.out.println("total salary is " + totalSalary);
+
+
+// Q. Enter table of n number taken from user
+
+        // System.out.println("enter number");
+        // int n = sc.nextInt();
+        // System.out.println("table of "+n+ " is");
+        // for(int i= 1; i<=10; i++){
+        //     System.out.println(n*i);
+        // }
     
-    // int x=5;
-    // int y=5;
-    // while((x=5)==y)
-    // {
-    //     System.out.println("Hello");
-    //     x++;
-    //     y++;
-    // }
-    /*here the output will get print only one time cause
-      x==5 will always be 5 in line 88  */ 
 
-//sum of n numbers -->      
 
-    //   int n = sc.nextInt();
-    //   int sum = 0;
-    //   for(int i =1; i<=n; i++){
-    //        sum = sum+i;
-    //   }
-    // System.out.println("sum of n numbers is "+ sum);
-
-//Fahrenheit to Celsius Table
-
-/*Q. Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W),
- you need to convert all Fahrenheit values from Start to End at the gap of W, into their
-  corresponding Celsius values and print the table.*/
-  
-    int startF = sc.nextInt();  // startF --> starting value of fahrenheit
-    int endF = sc.nextInt();  // endF --> end value of fahrenheit
-    int g = sc.nextInt();   //gap 
-    double c;
-        while(startF <= endF){
-             c = (startF-32)* 5/9;
-             //int d = (int)c;
-             System.out.println(startF+ " "+c);
-             startF= startF + g;
-        }
-    
-}
+    }
 }
--- Conditions/src/Prime.java
+++ Conditions/src/Prime.java
@@ -1,48 +1,77 @@
 import java.util.*;
 
 public class Prime {
-    // prime number program but not fully optimised i.e cant work on exception like 11,33 etc 
+    // prime number program but not fully optimised i.e cant work on exception like
+    // 11,33 etc
     // public static void main(String arg[]){
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.println("Enter number");
-    //     int n = sc.nextInt();
-      
-    //     if(n==1){
-    //         System.out.println("number is nor prime nor composite"); 
-    //     }
-    //     else if(n%2==1){
-    //         System.out.println("Number is prime number");
-    
-    //     }
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter number");
+    // int n = sc.nextInt();
 
-    //     else{
-    //         System.out.println("This number is not prime number");
-    //     }
+    // if(n==1){
+    // System.out.println("number is nor prime nor composite");
     // }
-  
- // final fully optimised code for prime number
-    public static void main(String arg[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter number");
-        int n = sc.nextInt();
-        boolean result = primeNumber(n);
-        sc.close();
-        if(result){
-            System.out.println("number is prime number");
-        }else{
-            System.out.println("number is not prime number");
-        }
-    }
- 
-//new function for conditions
-public static boolean primeNumber(int n){
-    if (n<=1)return false ;
-    if (n==2 || n== 3) return true;
-    if (n%2 == 0) return false ;
-    for ( int i=3; i <=n; i+=2){
-        if (n%i==0) return false; 
-    }
-     return true;
-}
-}
+    // else if(n%2==1){
+    // System.out.println("Number is prime number");
 
+    // }
+
+    // else{
+    // System.out.println("This number is not prime number");
+    // }
+    // }
+
+    // final fully optimised code for prime number
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("enter number");
+        // int n = sc.nextInt();
+        // boolean result = primeNumber(n);
+        // sc.close();
+        // if(result){
+        // System.out.println("number is prime number");
+        // }else{
+        // System.out.println("number is not prime number");
+        // }
+        // }
+
+        // //new function for conditions
+        // public static boolean primeNumber(int n){
+        // if (n<=1)return false ;
+        // if (n==2 || n== 3) return true;
+        // if (n%2 == 0) return false ;
+        // for ( int i=3; i <=n; i+=2){
+        // if (n%i==0){
+        // return false;
+        // }
+        // }
+        // return true;
+
+        // Q. prime number with different approach
+
+        // System.out.println("enter number");
+        // int n = sc.nextInt();
+        // int div = 2;
+        // boolean isPrime = true;
+        // while (div <= n - 1) { // we can also take n/2 here
+        // // we took n-1 cause if divisor= n; then it'll print as composite number
+        // // according to
+        // // the condition
+        // if (n % div == 0) {
+        // isPrime = false;
+        // System.out.println(n + " is not a prime number");
+        // break;
+        // }
+        // div = div + 1; // or simply div++
+        // }
+        // if (isPrime == true) { // if(isPrime) this is also right as after end of loop
+        // isPrime is always true
+        // // so; even if we coment out this if it'll always true so o/p will print
+        // prime number
+        // System.out.println(n + " is prime number");
+        // }
+
+        
+
+    }
+}
