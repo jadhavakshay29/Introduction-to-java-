893e0bae495a05f909e943df4339dc269c1ee330
jadhavakshay29 <akshayjadhavaj2911@gmail.com>
2022-08-25 00:19:56 +0530

    pyramidPatterns

--- introToJava/src/Practice.java
+++ introToJava/src/Practice.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import javax.annotation.processing.SupportedOptions;
+
 public class Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -317,16 +319,173 @@ public class Practice {
         //     System.out.println();
         // }
 
-        int n=sc.nextInt();
-        for(int i=1; i<=n; i++){
-            int p = i;
-            for(int j=1; j<=i; j++){
-                char alpha = (char)('A'+p-1);
-                System.out.print(alpha+" ");
-                p++;
-            }
-            System.out.println();
-        }
+        // int n=sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     int p = i;
+        //     for(int j=1; j<=i; j++){
+        //         char alpha = (char)('A'+p-1);
+        //         System.out.print(alpha+" ");
+        //         p++;
+        //     }
+        //     System.out.println();
+        // }
+
+//imp nd somewhat different one from others
+
+//Q. 
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n; i++){
+        //     char alpha = (char)('A'+n-i);  //main logic behind soln
+        //     for(int j= 1; j<=i; j++){
+        //         System.out.print(alpha);   //thid also
+        //         alpha = (char) (alpha + 1);   //and this one 
+                
+        //     }
+        //     System.out.println();
+        // }
+
+//mirror patterns
+
+        //  int n = sc.nextInt();
+        //  //outer loop for rows        
+        //  for(int i = 1; i<=n; i++){
+        //     //inner loop for spaces
+        //      for(int j=1; j<=n-i; j++){
+        //          System.out.print(" ");
+        //      }
+        //      //outer loop for value printing 
+        //      for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //      }
+        //      System.out.println();
+        //  }
+
+        // int n = sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     for(int j= 1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+// inverted triangle
+
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n; i++){
+        //     for(int j=1; j<=n-i+1; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+//Q. 4444
+// 333
+// 22
+// 1
+        // int n= sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i+1; j++){
+        //         System.out.print(n-i+1);
+        //     }
+            
+        //     System.out.println();
+        // }
+
+//different approach
+
+        //  int n = sc.nextInt();
+        //  for(int i=n; i>=1;i--){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        //  }
+
+
+//Q.
+    //      1 
+    //     121
+    //    12321
+    //   1234321
+    //  123454321
+
+        // int n = sc.nextInt();
+           //loop for rows
+        // for(int i = 1; i<=n; i++){
+            //loop for spaces
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+           //loop for left half of pattern
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(j);
+        //     }
+           //loop for right half of pattern
+        //     for(int j=i-1; j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+    //      * 
+    //     ***
+    //    *****
+    //   *******
+    //  *********
+        // int n= sc.nextInt();
+        // //loop for rows
+        // for(int i=1; i<=n; i++){
+        //     //loop for spaces
+        //     for(int j=1; j<=n-i; j++){
+        //           System.out.print(" ");
+        //     }
+        //     //loop to print left half 
+        //     for(int j=1; j<=i; j++){
+        //           System.out.print("*");
+        //     }
+        //     //loop to print right half 
+        //     for(int j=i-1; j>=1; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+//Q.  1 
+//   232
+//  34543
+// 4567654        
+        // int n = sc.nextInt();
+              //loop for rows
+        // for(int i=1; i<=n; i++){
+              //loop for spaces
+        //     for(int j=1; j<n-i; j++){
+        //         System.out.print(" ");
+               
+        //       }
+        // int p =i; 
+              //loop for left side of numbers to print
+        // for(int j=1; j<=i; j++){
+        //     System.out.print(p);
+        //     p++;
+        // }   
+             //loop for right sid to print
+        // for(int j=(2*i-2); j>=i; j--){
+        //     System.out.print(j);
+        // }
+        // System.out.println();
+            
+        // }
+
 
     }
 }
