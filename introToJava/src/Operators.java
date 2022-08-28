29c60bc2822e5fa797436f56c201648d629e47f2
jadhavakshay29 <akshayjadhavaj2911@gmail.com>
2022-08-28 12:30:38 +0530

    operators

--- introToJava/src/Operators.java
+++ introToJava/src/Operators.java
@@ -1,18 +1,18 @@
 public class Operators {
     public static void main(String[] arg){
-         int a= 60;
-         int b= 20;
+         int a= 10;
+         int b= -20;
          int c= 1;
             // AND oprator
-         System.out.println(a&b); 
+         System.out.println("AND " + (a&b)); 
             // OR operator
-         System.out.println(a|b);
+         System.out.println("OR "+(a|b));
            // EXOR operator
-         System.out.println(a^b);
+         System.out.println("XOR "+(a^b));
            //NOT operator
-         System.out.println(~a);
+         System.out.println("NOT of a "+ (~a));
            //NOT formula = -(x+1) 
-         System.out.println(~c);
-         System.out.println(~(~c));
+         System.out.println("NOT of c "+ (~c));
+         System.out.println("NOT NOT of c "+ (~(~c)));
     }
 }
