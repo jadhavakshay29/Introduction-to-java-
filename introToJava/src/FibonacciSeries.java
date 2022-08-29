3661ef36b123becb969b444a9809fbbd5cc4ed15
jadhavakshay29 <akshayjadhavaj2911@gmail.com>
2022-08-29 10:42:08 +0530

    operators

--- introToJava/src/FibonacciSeries.java
+++ introToJava/src/FibonacciSeries.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-    //   Scanner sc = new Scanner(System.in);  
+      Scanner sc = new Scanner(System.in);  
     //   System.out.println("enter nth number");
     //   int n = sc.nextInt();
     //   System.out.println("enter number to be find");
     //   int num = sc.nextInt();
     //   int a = 0;
     //   int b = 1, c;
-    // //   System.out.println(" fibonschhi series is ");
+    //   System.out.println(" fibonschhi series is ");
  
     //   while(a <n){
     //       System.out.println(a);
@@ -30,21 +30,21 @@ public class FibonacciSeries {
 
 //Q. print n number of fibonacci series
 
-      Scanner sc = new Scanner(System.in);  
-      System.out.println("enter nth number");
-      int n = sc.nextInt();
+    //   Scanner sc = new Scanner(System.in);  
+    //   System.out.println("enter nth number");
+    //   int n = sc.nextInt();
 
-      int a = 0;   // took a=0,b=1 cause first numbers of series are 0 and 1 
-      int b = 1, c;
-     System.out.println(" fibonschhi series is ");
+    //   int a = 0;   // took a=0,b=1 cause first numbers of series are 0 and 1 
+    //   int b = 1, c;
+    //  System.out.println(" fibonschhi series is ");
  
-      while(a <n){
-          System.out.println(a);
-          c = a + b;
-          a = b;    //took next number as a and b 
-          b = c;    
-      }
-       System.out.println("thank you");
+    //   while(a <n){
+    //       System.out.println(a);
+    //       c = a + b;
+    //       a = b;    //took next number as a and b 
+    //       b = c;    
+    //   }
+    //    System.out.println("thank you");
     }
 }
 
--- introToJava/src/Operators.java
+++ introToJava/src/Operators.java
@@ -1,18 +1,68 @@
+import java.rmi.StubNotFoundException;
+import java.util.*;
+
 public class Operators {
     public static void main(String[] arg){
-         int a= 10;
-         int b= -20;
-         int c= 1;
-            // AND oprator
-         System.out.println("AND " + (a&b)); 
-            // OR operator
-         System.out.println("OR "+(a|b));
-           // EXOR operator
-         System.out.println("XOR "+(a^b));
-           //NOT operator
-         System.out.println("NOT of a "+ (~a));
-           //NOT formula = -(x+1) 
-         System.out.println("NOT of c "+ (~c));
-         System.out.println("NOT NOT of c "+ (~(~c)));
+      Scanner sc = new Scanner(System.in);
+        //  int a= 10;
+        //  int b= -20;
+        //  int c= 1;
+        //     // AND oprator
+        //  System.out.println("AND " + (a&b)); 
+        //     // OR operator
+        //  System.out.println("OR "+(a|b));
+        //    // EXOR operator
+        //  System.out.println("XOR "+(a^b));
+        //    //NOT operator
+        //  System.out.println("NOT of a "+ (~a));
+        //    //NOT formula = -(x+1) 
+        //  System.out.println("NOT of c "+ (~c));
+        //  System.out.println("NOT NOT of c "+ (~(~c)));
+        //    //Bitwise left shft opeartion 
+        //  System.out.println("Bitwise left shift operation of 'a' by 1 is " + (a<<1));
+        //     // Bitwise right shift operation 
+        //  System.out.println("Bitwise right shift operation of 'a' by 1 is " + (a>>1));
+
+//Assignment operators
+        
+          // int a = 10;
+          //  // a = a + 1;
+          // System.out.println("+= " + ( a += 1));
+          // // a = a - 1;
+          // System.out.println("-= " + ( a -= 1));
+          // // a = a * 1;
+          // System.out.println("*= " + ( a *= 1));
+          // // a = a ^ 1;
+          // System.out.println("^= " + ( a ^= 1));
+          // // a = a << 1;
+          // System.out.println("<<= " + ( a <<= 1));
+          // // a = a >> 1;
+          // System.out.println(">>= " + ( a >>= 1));
+          
+        // int a = 10;
+        //    // a++ = 10 not 11 as it is post increment operator
+        // int b = a++ * 5; 
+        // System.out.println(b);
+
+
+/*Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+ *   F(n) = F(n-1) + F(n-2), 
+    Where, F(1) =  0, 
+           F(2) = 1
+ */
+          System.out.println("enter nth number ");
+          int n = sc.nextInt();
+          // System.out.println("number to find at position ");
+          // int findNum = sc.nextInt();
+          int a = 0;
+          int b = 1, c;
+          //we took i = 2 cause , we already declared a nd b i.e. first two numbers
+          for(int i =2; i<= n ; i++){
+            // System.out.print(a+" ");
+              c = a+b; 
+              a = b;
+              b = c;
+          }
+          System.out.println(a);
     }
 }
